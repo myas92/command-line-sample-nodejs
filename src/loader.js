a6241d315118b188044d4c4c52f6b3a14b492b0c
mohammadyaser <m.y.ahmadi22@gmail.com>
2021-12-04 11:01:59 +0330

    added nested command sample

--- src/loader.js
+++ src/loader.js
@@ -1,6 +1,7 @@
 'use strict';
 const yargs = require('yargs');
-
+// yargs.alias('h', 'help');
 require('./commands/print-command');
+require('./commands/nested-command');
 
 exports = yargs.parse();
